71783bc10bb611ea61ac079d5a3089deee4d502f
Izabella Chianca Simonetti <izabellayeh@gmail.com>
2020-07-17 23:19:41 -0300

    EntregaSemana8

---
+++ aula05_spring_mvc_boot_hibernate_jpa/src/main/java/br/com/bossini/aula05_spring_mvc_boot_hibernate_jpa/Aula05SpringMvcBootHibernateJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.bossini.aula05_spring_mvc_boot_hibernate_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula05SpringMvcBootHibernateJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula05SpringMvcBootHibernateJpaApplication.class, args);
+	}
+
+}
---
+++ aula05_spring_mvc_boot_hibernate_jpa/src/test/java/br/com/bossini/aula05_spring_mvc_boot_hibernate_jpa/Aula05SpringMvcBootHibernateJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bossini.aula05_spring_mvc_boot_hibernate_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula05SpringMvcBootHibernateJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
